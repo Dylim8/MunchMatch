8fff4811defcc94bbe1fb1d986df7c51eb6b152f
Dylim8 <50117181+Dylim8@users.noreply.github.com>
2023-10-17 20:09:16 -0700

    Add Xcode Project

---
+++ MunchMatch/MunchMatch/MunchMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MunchMatchApp.swift
+//  MunchMatch
+//
+//  Created by Dylan  Lim on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct MunchMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
