ed365fd67945cdc936a160d15ebe6a6a882f2c11
Dylim8 <50117181+Dylim8@users.noreply.github.com>
2023-10-17 23:01:42 -0700

    .

--- MunchMatch/MunchMatch/ContentView.swift
+++ MunchMatch/MunchMatch/ContentView.swift
@@ -6,11 +6,38 @@
 //
 
 import SwiftUI
+ 
+//Starting Page
 
 struct ContentView: View {
+    @State private var speed = 2.0
+    @State private var isEditing = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color(red: (244/255), green: (237/255), blue: (235/255))
+                        .ignoresSafeArea()
+            VStack{
+                Slider(
+                        value: $speed,
+                        in: 2...10,
+                        step: 1
+                    ) {
+                        Text("Speed")
+                    } minimumValueLabel: {
+                        Text("2")
+                    } maximumValueLabel: {
+                        Text("10")
+                    } onEditingChanged: { editing in
+                        isEditing = editing
+                    }
+                Text("Party Size: \(speed)")
+                        .foregroundColor(isEditing ? .red : .blue)
+                
+                Button("Create A Party") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }.padding()
+            }
+        }
     }
 }
 
@@ -19,3 +46,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
